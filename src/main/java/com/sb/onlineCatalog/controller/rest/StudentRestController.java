9ef599393aa02d8323fbc88a4bcfa2ffaaa7bf63
Bota Sorina <crisansorinageorgiana@yahoo.com>
2020-07-18 14:00:22 +0300

    rest3

--- src/main/java/com/sb/onlineCatalog/controller/rest/StudentRestController.java
+++ src/main/java/com/sb/onlineCatalog/controller/rest/StudentRestController.java
@@ -19,19 +19,32 @@ public class StudentRestController {
     public ResponseEntity<List<Student>> getAllStudents() {
         return ResponseEntity.ok(studentRepository.findAll());
     }
+
     @GetMapping("/student/{id}")
     public ResponseEntity<Student> getStudent(@PathVariable Integer id) {
         return ResponseEntity.of(studentRepository.findById(id));
     }
+
     @PostMapping("/student/create")
-    public ResponseEntity<Student>createStudent(@RequestBody Student student){
+    public ResponseEntity<Student> createStudent(@RequestBody Student student) {
         studentRepository.save(student);
         return ResponseEntity.ok(student);
     }
+
     @PatchMapping("/student/edit")
-    public ResponseEntity<Student>editStudent(@RequestBody Student student){
+    public ResponseEntity<Student> editStudent(@RequestBody Student student) {
         studentRepository.save(student);
         return ResponseEntity.ok(student);
+    }
+
+    @DeleteMapping("/student/{id}")
+    public ResponseEntity<Student> deleteStudent(@PathVariable Integer id) {
+        try {
+            studentRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
